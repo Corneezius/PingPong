34f2ebc025032672eb4e1b58df8f15f9b8dca590
Corneezius <cfr2116@columbia.edu>
2016-08-19 15:17:08 -0400

    add js applying pingpong rules

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $("#inputForm").submit(function(event) {
+    event.preventDefault();
+    $("li").remove();
+var array = [];
+var num = ($("#input").val());
+
+for ( var i = 1; i <= 15; i++) {
+  array.push(i);
+
+  if (i % 3 === 0 && i % 15 === 0) {
+  array.pop(i);
+  array.push("pingpong");
+  }
+  else if (i % 3 === 0 ) {
+  array.pop(i);
+  array.push("ping");
+  }
+  else if (i % 5 === 0 ) {
+  array.pop(i);
+  array.push("pong");
+  }
+ }
+
+console.log(array);
+for (var i = 0; i < array.length; i++) {
+      $(".output").append('<li>' + array[i] + '</li>');
+    }
+
+    $("li").show();
+
+
+   });
+  });
