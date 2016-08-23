8c9198a4ef51e94e55af90f9d7b84077a47ef916
Cornelius <crangev5@gmail.com>
2016-08-23 10:59:12 -0400

    fix indentation in js file

--- js/script.js
+++ js/script.js
@@ -2,28 +2,28 @@ $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
     $("li").remove();
-var array = [];
-var num = ($("#input").val());
+    var array = [];
+    var num = ($("#input").val());
 
-for ( var i = 1; i <= num; i++) {
-  array.push(i);
+    for ( var i = 1; i <= num; i++) {
+      array.push(i);
 
-  if (i % 15 === 0) {
-  array.pop(i);
-  array.push("pingpong");
-  }
-  else if (i % 3 === 0 ) {
-  array.pop(i);
-  array.push("ping");
-  }
-  else if (i % 5 === 0 ) {
-  array.pop(i);
-  array.push("pong");
-  }
- }
+      if (i % 15 === 0) {
+        array.pop(i);
+        array.push("pingpong");
+      }
+      else if (i % 3 === 0 ) {
+        array.pop(i);
+        array.push("ping");
+      }
+      else if (i % 5 === 0 ) {
+        array.pop(i);
+        array.push("pong");
+      }
+    }
 
-console.log(array);
-for (var i = 0; i < array.length; i++) {
+  
+    for (var i = 0; i < array.length; i++) {
       $(".output").append('<li>' + array[i] + '</li>');
     }
 
