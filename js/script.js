ebc4a6b7f05b8662806359aab543656bd8dec8ba
Cornelius <crangev5@gmail.com>
2016-08-23 11:24:49 -0400

    refactor code to separate backend and user interface logic

--- js/script.js
+++ js/script.js
@@ -2,32 +2,33 @@ $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
     $("li").remove();
-    var count = [];
-    var limit = ($("#input").val());
+    var userInput = ($("#input").val());
+    var result = counter(userInput);
 
-    for ( var i = 1; i <= limit; i++) {
-
-      if (i % 15 === 0) {
-        count.push("pingpong");
-      }
-      else if (i % 3 === 0 ) {
-        count.push("ping");
-      }
-      else if (i % 5 === 0 ) {
-        count.push("pong");
-      }
-      else {
-        count.push(i);
-      }
+    for (var i = 0; i < result.length; i++) {
+      $(".output").append('<li>' + result[i] + '</li>');
     }
-
-
-    for (var i = 0; i < count.length; i++) {
-      $(".output").append('<li>' + count[i] + '</li>');
-    }
-
-    $("li").show();
-
-
-   });
   });
+});
+
+var counter = function (limit) {
+  var count = [];
+
+  for ( var i = 1; i <= limit; i++) {
+
+    if (i % 15 === 0) {
+      count.push("pingpong");
+    }
+    else if (i % 3 === 0 ) {
+      count.push("ping");
+    }
+    else if (i % 5 === 0 ) {
+      count.push("pong");
+    }
+    else {
+      count.push(i);
+    }
+  }
+
+  return count;
+}
